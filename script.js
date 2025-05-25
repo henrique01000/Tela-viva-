9597dc7df303b195b28e573ff62764c3a965a9d2
henrique01000 <henriquesilva848486@gmail.com>
2025-05-25 16:29:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// Função para mostrar ou esconder a senha
-function verSenha(idCampo) {
-  const campo = document.getElementById(idCampo);
-  if (campo.type === "password") {
-    campo.type = "text";
-  } else {
-    campo.type = "password";
-  }
+function mostrarSenha(id) {
+  const campo = document.getElementById(id);
+  campo.type = campo.type === 'password' ? 'text' : 'password';
+}
+
+function fazerLogin() {
+  alert("Login realizado com sucesso!");
+  window.location.href = "index.html";
+  return false;
+}
+
+function cadastrarUsuario() {
+  alert("Cadastro realizado com sucesso!");
+  window.location.href = "index.html";
+  return false;
 }
