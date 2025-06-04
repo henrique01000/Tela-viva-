bc844cb3aaac9e30e8557e6b613e936c884f6b40
henrique01000 <henriquesilva848486@gmail.com>
2025-06-04 14:09:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// Mostrar lista de favoritos e continuar assistindo na tela
-window.onload = function () {
-  const fav = JSON.parse(localStorage.getItem('favoritos')) || [];
-  const lista = document.getElementById("listaFavoritos");
-  if (lista && fav.length > 0) {
-    fav.forEach(nome => {
-      const li = document.createElement("li");
-      li.textContent = nome;
-      lista.appendChild(li);
-    });
-  }
 
-  const assistindo = localStorage.getItem('assistindo');
-  const filmeAssistindo = document.getElementById("filmeAssistindo");
-  if (assistindo && filmeAssistindo) {
-    filmeAssistindo.textContent = assistindo;
-  }
-};
